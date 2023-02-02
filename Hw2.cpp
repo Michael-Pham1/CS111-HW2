58166135155fb9153481653b3f1cec2920fddb4b
Michael-Pham1 <47127433+Michael-Pham1@users.noreply.github.com>
2023-02-01 22:58:23 -0800

    Did stuff Co-authored-by: Thomas Merritt <ThomasMerritt@users.noreply.github.com>

--- Hw2.cpp
+++ Hw2.cpp
@@ -1,8 +1,98 @@
 #include <iostream>
+#include <string>
+#include <cmath>
 
 using namespace std;
 
+/*
+TODO
+GCD function
+Large modulo calculator
+Parse encrypted message
+decrypt each message
+Print out message
+Word bank
+*/
+
+/* Values
+p,q = 61, 73
+e = 7
+n = 4453
+Totient(n) = (p-1)(q-1) = 60 * 72 = 4320
+d = e^-1 mod (totient) = 7^-1 mod 4320 = 3703
+*/
+bool isPrime(int p);
+int findDivisor(int n);
+int eulersTotient(int p, int q);
+int invMod(int e, int totient);
+
 int main(){
-    cout <<"hi";
+    int e, n, m, p, q, totient, d;
+    string message, temp;
+
+    cout << "Enter a value for e: ";
+    getline(cin, temp);
+    e = stoi(temp);
+
+    cout << "Enter a value for n: ";
+    getline(cin, temp);
+    n = stoi(temp);
+
+    cout << "Enter a value for m : ";
+    getline(cin, temp);
+    m = stoi(temp);
+
+    cout << "Enter the message: ";
+    getline(cin, message);
+    //cout << e << n << m;
+    //cout << isPrime(71);
+    //cout << findDivisor(n);
+    p = findDivisor(n);
+    cout << "p: " << p << endl;
+    q = n / p;
+    cout << "q: " << q << endl;
+    totient = eulersTotient(p, q);
+    cout << "totient: " << totient << endl;
+    d = invMod(e, totient);
+    cout << "d: " << d << endl;
     return 0;
+}
+
+//Checks if number is prime
+bool isPrime(int p){
+    for(int i = 2; i < p; i++){
+        if(p % i == 0){
+            return false;
+        }
+    }
+    return true;
+}
+
+//Finds divisor
+int findDivisor(int n){
+    for(int i = 2; i < ceil(n / 2); i++){
+        if(isPrime(i)){
+            if(n % i == 0){
+                return i;
+            }
+        }
+    }
+    return -1;
+}
+
+int eulersTotient(int p, int q){
+    return((p-1) * (q-1)); 
+}
+
+int invMod(int e, int totient){
+    int res = 0;
+    for (int i = 1; i < 1000; i++) {
+        for (int j = 1; j < totient; j++) {
+            if (totient * i == (e * j) - 1) {
+                res = j;
+                break;
+            }
+        }
+    }
+    return res;
 }
